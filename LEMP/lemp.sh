0229b25b9154dbde624bdf0f2f40b29d0e51b92b
anamunlam <anam@unlam.ac.id>
2017-08-08 10:16:18 +0800

    Create lemp.sh

---
+++ LEMP/lemp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Description: LEMP Installer Script
+#
+# Copyright (C) 2017 Anam <anam@ahka.net>
+#
+# URL: http://ahka.net
+#
+
+OS=$(head -n1 /etc/issue | cut -f 1 -d ' ')
+VER=$(cat /etc/debian_version|grep -o [0-9]|head -n1)
+echo "$OS $VER"
+exit
