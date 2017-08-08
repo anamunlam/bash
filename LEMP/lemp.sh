e9000e4162959b3681e27e3b4d6e44208a27b7e0
anamunlam <anam@unlam.ac.id>
2017-08-08 10:26:00 +0800

    Update lemp.sh

--- LEMP/lemp.sh
+++ LEMP/lemp.sh
@@ -7,6 +7,14 @@
 # URL: http://ahka.net
 #
 
+get_os() {
+    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+
+GETOS = $( get_os )
+
 OS=$(head -n1 /etc/issue | cut -f 1 -d ' ')
 VER=$(cat /etc/debian_version|grep -o [0-9]|head -n1)
 clear
